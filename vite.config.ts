7518d5cee78c7d24daaba05ce0bbb51cede1ada2
Ssechan <tpcksrns@gmail.com>
2025-01-15 23:55:24 +0900

    config: vitest 설치 및 vite.config.ts 수정
    - yarn 버전을 3.6.4로 설정했습니다.("packageManager": "yarn@3.6.4")
    - 테스트를 실행하기 위한 vitest를 설치
    - package.json에 test 스크립트 추가
    - vite.config.ts에 테스트 환경 설정 추가
    
    - ref: #6

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,27 @@ import svgrPlugin from 'vite-plugin-svgr';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgrPlugin()],
-  resolve: {
-    alias: [
-      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
-      {
-        find: '@components',
-        replacement: path.resolve(__dirname, 'src/components'),
-      },
-      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
-      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
-    ],
-  },
+export default defineConfig(({ mode }) => {
+  const isTest = mode === 'test';
+
+  return {
+    plugins: [react(), svgrPlugin()],
+    resolve: {
+      alias: [
+        { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+        {
+          find: '@components',
+          replacement: path.resolve(__dirname, 'src/components'),
+        },
+        { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+        { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+      ],
+    },
+    test: isTest
+      ? {
+          globals: true,
+          environment: 'jsdom',
+        }
+      : undefined,
+  };
 });
