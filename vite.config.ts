a4d082b87f157dca7695c8ea423b544f9c7c4c3b
Ssechan <tpcksrns@gmail.com>
2025-01-20 00:10:39 +0900

    fix: vite.config.ts 수정 및 워크 스페이스 삭제
    - ref: #6

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,25 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import path from 'path';
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const isTest = mode === 'test';
-
-  return {
-    plugins: [react(), svgrPlugin()],
-    resolve: {
-      alias: [
-        { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
-        {
-          find: '@components',
-          replacement: path.resolve(__dirname, 'src/components'),
-        },
-        { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
-        { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
-      ],
-    },
-    test: isTest
-      ? {
-          globals: true,
-          environment: 'jsdom',
-          setupFiles: ['./vitest.setup.ts'],
-        }
-      : undefined,
-  };
+export default defineConfig({
+  plugins: [react(), svgrPlugin()],
+  resolve: {
+    alias: [
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+    ],
+  },
+  test: {
+    include: ['src/**/*.test.{ts, tsx}'],
+    globals: true,
+    environment: 'jsdom',
+  },
 });
