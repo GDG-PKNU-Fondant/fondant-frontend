157f7931f6bcffa773786a7471fc616d34552a22
Ssechan <tpcksrns@gmail.com>
2025-01-20 23:46:42 +0900

    chore: 테스트 코드 삭제
    - ref: #3

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,5 @@
-import { useEffect, useState } from 'react';
-
 function App() {
-  const [user, setUser] = useState<{
-    firstName: string;
-    lastName: string;
-  } | null>(null);
-
-  useEffect(() => {
-    fetch('http://localhost:5173')
-      .then((response) => response.json())
-      .then((data) => {
-        setUser(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching user data:', error);
-      });
-  }, []);
-
-  if (!user) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      Welcome, {user.firstName}
-      {user.lastName}!
-    </div>
-  );
+  return <div>fondant</div>;
 }
 
 export default App;
