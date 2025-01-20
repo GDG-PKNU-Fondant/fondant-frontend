2e2dff5ebb582cb69153f93b6a9dc9b1e6706aea
Ssechan <tpcksrns@gmail.com>
2025-01-20 19:57:52 +0900

    config: msw의 브라우저 환경 통합
    - 브라우저 환경 API 모킹을 위한 woker 생성(browser.ts)
    - 개발 환경에서 woker 실행을 위한 main.tsx 수정(조건부 msw 시작)
    - worker.start()는 비동기 작업으로, 브라우저에 서비스 워커를 등록하는 과정이 포함 필요(.then)
    
    - ref: #3

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import '@styles/tailwind.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+  const { worker } = await import('./mocks/browser');
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+});
