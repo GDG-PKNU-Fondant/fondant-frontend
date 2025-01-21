ad0091914c6a939d12b6ca5e542250b3ee21a21f
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-01-21 13:17:02 +0900

    refactor: 중복된 export 키워드 제거
    - ref: #3

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,6 @@
 import { http, HttpResponse } from 'msw';
 
-export const handlers = [
+const handlers = [
   http.get('http://localhost:5173', () => {
     return HttpResponse.json({
       firstName: 'Fon',
--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,6 +1,6 @@
 import { setupServer } from 'msw/node';
 import handlers from '@mocks/handlers';
 
-export const server = setupServer(...handlers);
+const server = setupServer(...handlers);
 
 export default server;
