16d68cf5e3ee41ac5af9096ff5d9d56e2e7dff9d
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-01-21 13:27:08 +0900

    refactor: enableMocking 함수 선언을 화살표 함수 표현식으로 변경
    - ref: #3

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import '@styles/tailwind.css';
 
-async function enableMocking() {
+const enableMocking = async () => {
   if (process.env.NODE_ENV !== 'development') {
     return Promise.resolve();
   }
   const { default: worker } = await import('@mocks/browser');
 
   return worker.start();
-}
+};
 
 enableMocking().then(() => {
   createRoot(document.getElementById('root')!).render(
