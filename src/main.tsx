5cd8af8a40b5b0a834b748a58305a42d2cf05cfa
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-01-21 13:24:22 +0900

    fix: @mocks/browser 모듈의 default export 명시적 import 수정
    - ref: #3

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
     return Promise.resolve();
   }
-  const { worker } = await import('@mocks/browser');
+  const { default: worker } = await import('@mocks/browser');
 
   return worker.start();
 }
