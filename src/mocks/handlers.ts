b81bba4760395033c26d326bcbfc7ffd11d1b309
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-01-23 04:49:23 +0900

    chore: mock 데이터 및 msw 핸들러 작성
    - ref: #4

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,36 @@
-import { http, HttpResponse } from 'msw';
+import { http } from 'msw';
+
+export const mockSlides = [
+  {
+    id: 0,
+    thumbnailUrl:'https://images.unsplash.com/photo-1452968011964-24f8831c43c3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    redirectUrl: '',
+  },
+  {
+    id: 1,
+    thumbnailUrl:'https://images.unsplash.com/photo-1541781550486-81b7a2328578?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    redirectUrl: '',
+  },
+  {
+    id: 2,
+    thumbnailUrl:'https://images.unsplash.com/photo-1581343659198-abab39bb36aa?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    redirectUrl: '',
+  },
+  {
+    id: 3,
+    thumbnailUrl:'https://images.unsplash.com/photo-1713274783669-631543642a61?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    redirectUrl: '',
+  },
+  {
+    id: 4,
+    thumbnailUrl:'https://images.unsplash.com/photo-1566760375903-061dfd31c175?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    redirectUrl: '',
+  },
+];
 
 const handlers = [
-  http.get('http://localhost:5173', () => {
-    return HttpResponse.json({
-      firstName: 'Fon',
-      lastName: 'dant',
-    });
+  http.get('/api/carousel', () => {
+    return new Response(JSON.stringify(mockSlides), { status: 200 });
   }),
 ];
 
