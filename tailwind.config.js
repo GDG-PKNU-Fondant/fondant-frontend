c1b18f31c3ea95c9e30fdf54a4c14aba4da17aa3
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-01-24 00:35:06 +0900

    chore: Tailwind CSS 커스텀 색상 추가
    - ref: #14

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#FFFCF8',
+        beige: {
+          primary: '#EBD8CB',
+          secondary: '#F1E3D9',
+          tertiary: '#FDF4ED',
+        },
+        brown: {
+          primary: '#613E29',
+          secondary: '#BC8462',
+          tertiary: '#DEBBA3',
+        },
+        pink: '#FF80A6',
+      },
+    },
   },
   plugins: [],
 };
