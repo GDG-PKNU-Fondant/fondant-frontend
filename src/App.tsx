b0f736c6535625033a72f321f189b47349f82660
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-02-15 03:46:08 +0900

    feat: router 적용
    - ref: #20

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
-function App() {
-  return <div>fondant</div>;
-}
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '@pages/Home';
+import Category from '@pages/Category';
+import Search from '@pages/Search';
+import Wish from '@pages/Wish';
+import My from '@pages/My';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/wish" element={<Wish />} />
+        <Route path="/my" element={<My />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
