117ac4741e7a69fc7589108f78483bd4859d6959
seun1077 <kseun1118@naver.com>
2025-02-16 18:52:21 +0900

    feat: MarketProfile 컴포넌트 타입 정의
    - ref: #24

---
+++ src/types/MarketProfile.ts
@@ -0,0 +1,8 @@
+export default interface MarketProfileProps {
+  profileImageUrl: string;
+  marketName: string;
+  description: string;
+  thumbnailUrls: string[];
+  rating: number;
+  likes: number;
+}
