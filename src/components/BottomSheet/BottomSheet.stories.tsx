0095cae72553d55974e88037c419a8c9af2ff704
Ssechan <tpcksrns@gmail.com>
2025-02-20 12:50:55 +0900

    docs: 하단 시트 스토리북 추가 작성
    - ref: #23

--- src/components/BottomSheet/BottomSheet.stories.tsx
+++ src/components/BottomSheet/BottomSheet.stories.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import BottomSheet from '.';
+import BottomSheet from '@components/BottomSheet';
+import SortSheet from '@components/BottomSheet/SortSheet';
+import MOCK_SORT_OPTIONS from '@mocks/constants/mockSortList'; // 정렬 옵션
 
 const meta: Meta<typeof BottomSheet> = {
   title: 'Components/BottomSheet',
@@ -30,6 +32,32 @@ export const Default: Story = {
         <BottomSheet isOpen={isOpen} onClose={() => setIsOpen(false)}>
           <p className="text-center">내용 추가</p>
         </BottomSheet>
+      </div>
+    );
+  },
+};
+
+export const SortBottomSheet: Story = {
+  render: (args) => {
+    const [isOpen, setIsOpen] = useState(args.isOpen ?? false);
+    const [selectedSort, setSelectedSort] = useState(
+      MOCK_SORT_OPTIONS[0].value,
+    );
+
+    return (
+      <div>
+        <button
+          onClick={() => setIsOpen(true)}
+          className="p-2 bg-blue-500 text-white rounded"
+        >
+          정렬 모달 열기
+        </button>
+        <SortSheet
+          isOpen={isOpen}
+          onClose={() => setIsOpen(false)}
+          onSelect={(option) => setSelectedSort(option)}
+          selectedOption={selectedSort}
+        />
       </div>
     );
   },
