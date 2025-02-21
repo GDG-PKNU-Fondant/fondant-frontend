a59440b0dea7bb56e179b6f71f8fdf193aafd81f
seun1077 <kseun1118@naver.com>
2025-02-21 19:07:23 +0900

    chore: MarketProfileCard 컴포넌트에서 사용할 mock 데이터 작성
    - ref: #24

---
+++ src/mocks/constants/mockMarketProfiles.ts
@@ -0,0 +1,45 @@
+const MOCK_MARKET_PROFILES = [
+  {
+    id: 0,
+    profileImageUrl:
+      'https://images.unsplash.com/photo-1555507036-ab1f4038808a?q=80&w=1926&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    marketName: '세찬 베이커리',
+    description: '맛있는 빵이 있는 곳',
+    thumbnailUrls: [
+      'https://images.unsplash.com/photo-1587241321921-91a834d6d191?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1623334044303-241021148842?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1613929231151-d7571591259e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ],
+    rating: 4.9,
+    likes: 987,
+  },
+  {
+    id: 1,
+    profileImageUrl:
+      'https://images.unsplash.com/photo-1558961363-fa8fdf82db35?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    marketName: '상윤이네 쿠키',
+    description: '상윤이가 직접 구운 맛있는 쿠키',
+    thumbnailUrls: [
+      'https://images.unsplash.com/photo-1590080874088-eec64895b423?q=80&w=1794&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1634188023615-7e08901193b6?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    ],
+    rating: 4.2,
+    likes: 1500,
+  },
+  {
+    id: 2,
+    profileImageUrl: '',
+    marketName: '강지원 명인 한과',
+    description: '명인의 한과를 맛보세요',
+    thumbnailUrls: [
+      '',
+      '',
+      '',
+      'https://cdn.pixabay.com/photo/2017/03/17/04/15/korean-traditional-sweets-2150895_1280.jpg',
+    ],
+    rating: 4.9,
+    likes: 10500,
+  },
+];
+
+export default MOCK_MARKET_PROFILES;
