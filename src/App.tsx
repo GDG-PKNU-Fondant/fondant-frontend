07eb315aadcbc96139eecb169e35ed8899e4ee97
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-02-24 03:54:48 +0900

    feat: 모바일 레이아웃 적용 및 헤더 배치
    - ref: #33

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,29 @@ import Category from '@pages/Category';
 import Search from '@pages/Search';
 import Wish from '@pages/Wish';
 import My from '@pages/My';
+import Header from '@components/Header';
 import BottomTab from '@components/BottomTab';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/category" element={<Category />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/wish" element={<Wish />} />
-        <Route path="/my" element={<My />} />
-      </Routes>
-      <BottomTab />
+      <div className="app-container">
+        <Header
+          onCartClick={() => {}}
+          onNotificationClick={() => {}}
+          onSearchClick={() => {}}
+        />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/wish" element={<Wish />} />
+            <Route path="/my" element={<My />} />
+          </Routes>
+        </div>
+        <BottomTab />
+      </div>
     </Router>
   );
 };
