19d7c1350540fddc3139d52858eec2e84b87c7e3
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-03-01 22:54:23 +0900

    refactor: formatPeopleCount 내 변수명 수정

--- src/utils/formatPeopleCount.ts
+++ src/utils/formatPeopleCount.ts
@@ -1,10 +1,10 @@
 const formatPeopleCount = (count: number): string => {
   if (count >= 10000) {
-    const formattedReviewer =
+    const formattedCount =
       count >= 100000
         ? Math.floor(count / 10000)
         : Math.floor(count / 1000) / 10;
-    return `${formattedReviewer}만`;
+    return `${formattedCount}만`;
   }
   return new Intl.NumberFormat().format(count);
 };
