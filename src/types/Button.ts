641687364625c040aec096ee9d1c49edee9b912c
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-03-12 04:21:41 +0900

    feature: 드롭다운 컴포넌트 구현
    - ref: #45

--- src/types/Button.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonHTMLAttributes } from 'react';
-
-export default interface ButtonProps
-  extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary' | 'secondary' | 'tertiary' | 'submit';
-  size?: 'small' | 'medium' | 'large';
-  block?: boolean;
-}
