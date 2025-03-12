04850c42376a264a64d831afce2d0c0c74fc5379
seun1077 <kseun1118@naver.com>
2025-03-12 15:17:12 +0900

    chore: PopularSearches mock 데이터 수정
    - ref: #41

--- src/mocks/constants/mockPopularSearches.ts
+++ src/mocks/constants/mockPopularSearches.ts
@@ -1,10 +1,8 @@
-const MOCK_POPULAR_SEARCHES = [
-  { rank: 1, keyword: '멋진빵', trend: 'up' },
-  { rank: 2, keyword: '김마루 귀여워', trend: 'down' },
-  { rank: 3, keyword: '김호두 사랑해', trend: 'neutral' },
-  { rank: 4, keyword: '으라차차', trend: 'neutral' },
-  { rank: 5, keyword: '치즈케이크', trend: 'neutral' },
-  { rank: 6, keyword: '오늘칼국수먹음', trend: 'neutral' },
-];
+const MOCK_POPULAR_SEARCHES = {
+  searches: [
+    { rank: 1, keyword: '오늘칼국수먹음', trend: 'neutral' }
+  ],
+  updateTime: 14,
+};
 
 export default MOCK_POPULAR_SEARCHES;
