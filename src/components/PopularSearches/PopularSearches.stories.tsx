b64d275a4df4a7eba50da1a4fcb23845c5d43839
seun1077 <kseun1118@naver.com>
2025-03-12 15:19:10 +0900

    docs: autodocs 분류 수정
    - ref: #41

--- src/components/PopularSearches/PopularSearches.stories.tsx
+++ src/components/PopularSearches/PopularSearches.stories.tsx
@@ -8,6 +8,23 @@ export default {
   title: 'Components/PopularSearches',
   component: PopularSearches,
   tags: ['autodocs'],
+  argTypes: {
+    searches: {
+      description: '인기 검색어 옵션을 설정합니다.',
+      control: 'object',
+    },
+    updateTime: {
+      description: '업데이트 시간을 설정합니다.',
+      control: 'number',
+    }
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: '인기 검색어를 나타내는 컴포넌트입니다.',
+      },
+    },
+  },
 } as Meta;
 
 const Template: StoryFn<PopularSearchesProps> = (args) => (
@@ -16,29 +33,18 @@ const Template: StoryFn<PopularSearchesProps> = (args) => (
 
 export const Default = Template.bind({});
 Default.args = {
-  searches: MOCK_POPULAR_SEARCHES,
+  searches: MOCK_POPULAR_SEARCHES.searches,
+  updateTime: MOCK_POPULAR_SEARCHES.updateTime,
 };
 
-export const WithRankChanges = Template.bind({});
-WithRankChanges.args = {
-  searches: [
-    { rank: 1, keyword: '김마루 귀여워', trend: 'up' },
-    { rank: 2, keyword: '멋진빵', trend: 'down' },
-    { rank: 3, keyword: '치즈케이크', trend: 'up' },
-    { rank: 4, keyword: '김호두 사랑해', trend: 'down' },
-    { rank: 5, keyword: '오늘칼국수먹음', trend: 'up' },
-    { rank: 6, keyword: '으라차차', trend: 'down' },
-  ],
+export const TrendUp = Template.bind({});
+TrendUp.args = {
+  searches: [{ rank: 1, keyword: '김마루 귀여워', trend: 'up' }],
+  updateTime: 3,
 };
 
-export const AllNeutral = Template.bind({});
-AllNeutral.args = {
-  searches: [
-    { rank: 1, keyword: '멋진빵', trend: 'neutral' },
-    { rank: 2, keyword: '김마루 귀여워', trend: 'neutral' },
-    { rank: 3, keyword: '김호두 사랑해', trend: 'neutral' },
-    { rank: 4, keyword: '으라차차', trend: 'neutral' },
-    { rank: 5, keyword: '치즈케이크', trend: 'neutral' },
-    { rank: 6, keyword: '오늘칼국수먹음', trend: 'neutral' },
-  ],
+export const TrendDown = Template.bind({});
+TrendDown.args = {
+  searches: [{ rank: 1, keyword: '멋진빵', trend: 'down' }],
+  updateTime: 21,
 };
