7df5eec838b26be8788aa4b41a88a8c26a631497
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-03-19 16:11:56 +0900

    fix: 하단 시트 동작 개선
    - ref: #50

--- src/components/BottomSheet/index.tsx
+++ src/components/BottomSheet/index.tsx
@@ -1,7 +1,13 @@
-import useModal from '@hooks/useModal';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
+import {
+  AnimatePresence,
+  motion,
+  PanInfo,
+  useDragControls,
+} from 'framer-motion';
+import useModal from '@hooks/useModal';
+import useBodyScrollLock from '@hooks/useBodyScrollLock';
 
 interface BottomSheetProps {
   sheetKey: string;
@@ -11,9 +17,29 @@ interface BottomSheetProps {
 const BottomSheet = ({ sheetKey, children }: BottomSheetProps) => {
   const { isModalOpen, closeModal } = useModal();
   const isBottomSheetOpen = isModalOpen(sheetKey);
+  const dragControls = useDragControls();
+  const { lockBodyScroll, unlockBodyScroll } = useBodyScrollLock();
 
   const sheetRef = useRef<HTMLDivElement | null>(null);
   const lastFocusedElementRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    const handleScrollLock = () => {
+      if (isBottomSheetOpen) {
+        lockBodyScroll();
+      } else {
+        unlockBodyScroll();
+      }
+    };
+
+    handleScrollLock();
+
+    return () => {
+      if (isBottomSheetOpen) {
+        unlockBodyScroll();
+      }
+    };
+  }, [isBottomSheetOpen, lockBodyScroll, unlockBodyScroll]);
 
   useEffect(() => {
     if (isBottomSheetOpen) {
@@ -25,6 +51,20 @@ const BottomSheet = ({ sheetKey, children }: BottomSheetProps) => {
       lastFocusedElementRef.current?.focus();
     }
   }, [isBottomSheetOpen]);
+
+  const startDrag = (event: React.PointerEvent) => {
+    dragControls.start(event);
+  };
+
+  const handleDragEnd = (
+    // @ts-expect-error: onDragEnd의 시그니처와 일치시키기 위해 남겨둠
+    event: MouseEvent | TouchEvent | PointerEvent,
+    info: PanInfo,
+  ) => {
+    if (info.offset.y > 50) {
+      closeModal(sheetKey);
+    }
+  };
 
   return createPortal(
     <AnimatePresence>
@@ -41,27 +81,26 @@ const BottomSheet = ({ sheetKey, children }: BottomSheetProps) => {
           <motion.div
             ref={sheetRef}
             tabIndex={-1}
-            className="bg-background w-full max-w-md rounded-t-2xl p-[8px] origin-bottom"
+            className="bg-background w-full max-w-[480px] rounded-t-2xl p-[8px] origin-bottom"
             onClick={(e) => e.stopPropagation()}
             initial={{ y: '100%' }}
             animate={{ y: 0 }}
             exit={{ y: '100%' }}
             transition={{ duration: 0.2, ease: 'easeOut' }}
             drag="y"
+            dragControls={dragControls}
+            dragListener={false}
             dragConstraints={{ top: 0, bottom: 100 }}
-            dragElastic={0}
-            onDragEnd={(_, info) => {
-              if (info.offset.y > 50) closeModal(sheetKey);
-            }}
+            dragElastic={0.5}
+            onDragEnd={handleDragEnd}
+            dragSnapToOrigin
           >
-            <div className="flex justify-center items-center mb-[12px]">
-              <button
-                type="button"
-                aria-label="닫기"
-                data-testid="bottom-sheet-close-button"
-                className="w-[40px] h-[4px] bg-beige-primary rounded-full cursor-pointer"
-                onClick={() => closeModal(sheetKey)}
-              />
+            <div
+              data-testid="bottom-sheet-close-button"
+              className="flex justify-center items-center pt-[4px] p-[12px]"
+              onPointerDown={startDrag}
+            >
+              <div className="w-[40px] h-[4px] bg-beige-primary rounded-full cursor-pointer" />
             </div>
             {children}
           </motion.div>
