3e06b28c8f451a2449a2213ed43cc677e9b9c604
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-03-19 15:20:32 +0900

    refactor: 옵션 변경 시트에 하드 코딩된 SVG 아이콘 분리
    - ref: #28

--- src/pages/Cart/components/ProductOptionSheetContent/index.tsx
+++ src/pages/Cart/components/ProductOptionSheetContent/index.tsx
@@ -4,6 +4,7 @@ import Button from '@components/Button';
 import QuantityController from '@components/QuantityController';
 import { ProductOption } from '@type/Product';
 import { CartItem, CartItemOption } from '@type/MarketCartCard';
+import RemoveIcon from '@assets/icons/remove.svg?react';
 
 interface ProductOptionSheetContentProps {
   item: CartItem;
@@ -40,18 +41,7 @@ const SelectedOption: React.FC<SelectedOptionProps> = ({
           className="text-brown-primary text-[13px] cursor-pointer p-[5px] mr-[-5px]"
           onClick={onRemove}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="9"
-            height="9"
-            viewBox="0 0 9 9"
-            fill="none"
-          >
-            <path
-              d="M7.50525 0.493111L4.50005 3.49831L1.49485 0.493111C1.36201 0.360273 1.18184 0.285645 0.993978 0.285645C0.806116 0.285645 0.625949 0.360273 0.493111 0.493111C0.360272 0.625949 0.285645 0.806117 0.285645 0.993979C0.285645 1.18184 0.360272 1.36201 0.49311 1.49485L3.49831 4.50005L0.49311 7.50525C0.360272 7.63809 0.285645 7.81826 0.285645 8.00612C0.285645 8.19398 0.360272 8.37415 0.493111 8.50699C0.625949 8.63983 0.806116 8.71445 0.993978 8.71445C1.18184 8.71445 1.36201 8.63983 1.49485 8.50699L4.50005 5.50178L7.50525 8.50699C7.63809 8.63983 7.81826 8.71445 8.00612 8.71445C8.19398 8.71445 8.37415 8.63983 8.50699 8.50699C8.63982 8.37415 8.71445 8.19398 8.71445 8.00612C8.71445 7.81826 8.63983 7.63809 8.50699 7.50525L5.50178 4.50005L8.50699 1.49485C8.63983 1.36201 8.71445 1.18184 8.71445 0.993978C8.71445 0.806116 8.63982 0.625949 8.50699 0.493111C8.37415 0.360273 8.19398 0.285645 8.00612 0.285645C7.81826 0.285645 7.63809 0.360273 7.50525 0.493111Z"
-              fill="#BC8462"
-            />
-          </svg>
+          <RemoveIcon />
         </button>
       </div>
       <div className="flex items-center justify-between">
