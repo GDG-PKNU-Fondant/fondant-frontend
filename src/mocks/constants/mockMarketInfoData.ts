78d1ecbd3c67036116c40268914e8a21f1ad4741
Ssechan <tpcksrns@gmail.com>
2025-03-20 03:33:40 +0900

    chore: MarketInfo 타입 명세 및 Mock Data 생성
    - ref: #43

---
+++ src/mocks/constants/mockMarketInfoData.ts
@@ -0,0 +1,13 @@
+const mockMarketInfoData = {
+  backgroundImage:
+    'https://img.freepik.com/free-photo/various-jelly-beans-blue-background_58702-2160.jpg?t=st=1742393339~exp=1742396939~hmac=fe737d28c1639bfda749dfa64f4e802f658a39c0d6821d6b32391c906a00f79d&w=2000',
+  thumbnail:
+    'https://images.unsplash.com/photo-1555507036-ab1f4038808a?q=80&w=3508&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  marketName: '소금소금소금빵',
+  title: '🥐 빵 인기 TOP 10',
+  description: '소금빵에 소금 많이요.',
+  tags: ['빠쌲큐싺', '와쟉와작'],
+  likes: 152,
+};
+
+export default mockMarketInfoData;
---
+++ src/types/MarketInfo.ts
@@ -0,0 +1,9 @@
+export default interface MarketInfoProps {
+  backgroundImage: string;
+  thumbnail: string;
+  marketName: string;
+  title?: string;
+  description: string;
+  tags: string[];
+  likes: number;
+}
