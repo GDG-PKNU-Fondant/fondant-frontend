133e5debea61e8fbafa7b00ebd3e34d453148b51
seun1077 <kseun1118@naver.com>
2025-03-23 05:34:10 +0900

    chore: 배송지 주소 mock 데이터 수정
    - rename:  mockUser -> mockDeliveryAddress 변경
    - label 추가
    - ref: #49

---
+++ src/mocks/constants/mockDeliveryAddress.ts
@@ -0,0 +1,9 @@
+const MOCK_DELIVERY_ADDRESS = {
+  label: '배송지 지정 이름',
+  name: '김마루',
+  phone: '010-0000-0000',
+  address: '부산광역시 남구 용소로 45 (국립부경대학교)',
+  detailAddress: '누리관 1111호 [우편번호]',
+};
+
+export default MOCK_DELIVERY_ADDRESS;
