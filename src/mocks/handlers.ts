c8ceed096cecb61a43d3e06f096d30afb2d5806d
seun1077 <kseun1118@naver.com>
2025-04-01 03:26:14 +0900

    chore: mock 주문 완료 응답 핸들러 추가
    - ref: #56

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,13 @@
 import { http } from 'msw';
 import MOCK_CAROUSEL_SLIDES from '@mocks/constants/mockCarouselSlides';
+import MOCK_ORDER_RESPONSE from '@mocks/constants/mockOrderResponse';
 
 const handlers = [
   http.get('/api/carousel', () => {
     return new Response(JSON.stringify(MOCK_CAROUSEL_SLIDES), { status: 200 });
+  }),
+  http.post('/api/orders', () => {
+    return new Response(JSON.stringify(MOCK_ORDER_RESPONSE), { status: 200 });
   }),
 ];
 
