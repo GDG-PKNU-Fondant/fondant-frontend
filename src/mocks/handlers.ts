96b785f3f4ba6113fd541ec77feb12127937a175
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-04-06 00:10:02 +0900

    chore: 상품 상세 페이지 구현을 위한 API 모킹
    - ref: #53

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,36 @@
 import { http } from 'msw';
-import MOCK_CAROUSEL_SLIDES from '@mocks/constants/mockCarouselSlides';
+import MOCK_PRODUCT_DETAILS from '@mocks/constants/mockProductDetails';
+import MOCK_REVIEWS from '@mocks/constants/mockReviews';
 
 const handlers = [
-  http.get('/api/carousel', () => {
-    return new Response(JSON.stringify(MOCK_CAROUSEL_SLIDES), { status: 200 });
+  http.get('/api/products/:productId', ({ params }) => {
+    const { productId } = params;
+    const product = MOCK_PRODUCT_DETAILS.find(
+      (p) => p.id === parseInt(productId as string, 10),
+    );
+
+    if (!product) {
+      return new Response(
+        JSON.stringify({ message: '해당 상품을 찾을 수 없습니다.' }),
+        {
+          status: 404,
+        },
+      );
+    }
+
+    return new Response(JSON.stringify(product), { status: 200 });
+  }),
+  http.get('/api/products/:productId/reviews', ({ params }) => {
+    const { productId } = params;
+    const productReviewsData = MOCK_REVIEWS[parseInt(productId as string, 10)];
+
+    if (!productReviewsData) {
+      return new Response(JSON.stringify([]), { status: 200 });
+    }
+
+    return new Response(JSON.stringify(productReviewsData.reviews), {
+      status: 200,
+    });
   }),
 ];
 
