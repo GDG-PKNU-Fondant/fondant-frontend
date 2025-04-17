c8873799502b99d2fc983ac5663bc203a87ba356
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-04-18 06:26:26 +0900

    fix: 테스트 코드 수정
    - ref: #59

--- src/components/Badge/Badge.test.tsx
+++ src/components/Badge/Badge.test.tsx
@@ -3,7 +3,7 @@ import { describe, it, expect } from 'vitest';
 import Badge from '@components/Badge';
 
 describe('Badge Component', () => {
-  it('Badge 렌더링 하고 자식 컴포넌트인 button이 있는지 확인', () => {
+  it('자식 컴포넌트가 정상적으로 렌더링 된다.', () => {
     render(
       <Badge type="alert">
         <button type="button">Cart</button>
@@ -12,27 +12,7 @@ describe('Badge Component', () => {
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
 
-  it('alert Badge의 올바른 렌더링 확인', () => {
-    render(
-      <Badge type="alert" count={1}>
-        <button type="button">Cart</button>
-      </Badge>,
-    );
-    const alertBadge = screen.getByTestId('badge');
-    expect(alertBadge).toBeInTheDocument();
-    expect(alertBadge).toHaveClass('bg-pink');
-  });
-
-  it('count Badge의 올바른 렌더링 확인', () => {
-    render(
-      <Badge type="count" count={5}>
-        <button type="button">Cart</button>
-      </Badge>,
-    );
-    expect(screen.getByText('5')).toBeInTheDocument();
-  });
-
-  it('Count Badge의 숫자가 99를 넘으면 99+로 적용', () => {
+  it('count type 배지의 숫자가 99를 넘으면 99+로 적용된다.', () => {
     render(
       <Badge type="count" count={120}>
         <button type="button">Cart</button>
@@ -41,9 +21,9 @@ describe('Badge Component', () => {
     expect(screen.getByText('99+')).toBeInTheDocument();
   });
 
-  it('Badge가 올바른 위치으로 렌더링 됨을 확인', () => {
+  it('배지가 올바른 위치에 렌더링 된다.', () => {
     render(
-      <Badge type="count" count={10} inset="auto 10px 10px auto">
+      <Badge type="count" count={10} position="bottom-right">
         <button type="button">Cart</button>
       </Badge>,
     );
@@ -51,10 +31,13 @@ describe('Badge Component', () => {
     const badge = screen.getByText('10');
     expect(badge).toBeInTheDocument();
 
-    expect(badge).toHaveStyle({ inset: 'auto 10px 10px auto' });
+    const badgeElement = screen.getByTestId('badge');
+    expect(badgeElement).toHaveClass(
+      'bottom-0 right-0 translate-y-1/3 translate-x-1/3',
+    );
   });
 
-  it('count 값이 0일 때 Badge가 렌더링되지 않음을 확인', () => {
+  it('count가 0일 때 배지가 렌더링 되지 않는다.', () => {
     render(
       <Badge type="count" count={0}>
         <button type="button">Cart</button>
