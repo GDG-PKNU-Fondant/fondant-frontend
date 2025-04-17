8e7b15f6d53e8bc40991b176b0e0fc16ebffdba0
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-04-18 06:54:19 +0900

    feat: PageHeader 컴포넌트 확장
    - ref: #53

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAtomValue } from 'jotai';
+import Badge from '@components/Badge';
+import { cartCountAtom } from '@stores/badgeState';
 import BackIcon from '@assets/icons/back.svg?react';
+import HomeIcon from '@assets/icons/home.svg?react';
+import SearchIcon from '@assets/icons/search.svg?react';
+import CartIcon from '@assets/icons/cart.svg?react';
 
 interface PageHeaderProps {
-  title: string;
+  title?: string;
+  showActionButtons?: boolean;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  showActionButtons = false,
+}) => {
   const navigate = useNavigate();
+  const cartCount = useAtomValue(cartCountAtom);
 
   return (
-    <header className="sticky top-0 left-0 right-0 w-full">
+    <header className="sticky top-0 left-0 right-0 w-full z-1">
       <div className="flex h-[60px] bg-background items-center justify-between px-[16px]">
         <button
           className="cursor-pointer"
@@ -25,6 +36,39 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
         >
           {title}
         </div>
+        {showActionButtons && (
+          <div className="flex gap-[16px]">
+            <button
+              className="cursor-pointer"
+              type="button"
+              onClick={() => {
+                navigate('/home');
+              }}
+            >
+              <HomeIcon />
+            </button>
+            <button
+              className="cursor-pointer"
+              type="button"
+              onClick={() => {
+                navigate('/search');
+              }}
+            >
+              <SearchIcon />
+            </button>
+            <button
+              className="cursor-pointer"
+              type="button"
+              onClick={() => {
+                navigate('/cart');
+              }}
+            >
+              <Badge type="count" count={cartCount}>
+                <CartIcon />
+              </Badge>
+            </button>
+          </div>
+        )}
       </div>
     </header>
   );
