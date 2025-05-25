0e425c03bc40c87f6ee16de4a9a36e18f4156a06
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-05-26 04:09:01 +0900

    feat: ProductList 컴포넌트 작성
    - ref: #66

---
+++ src/components/ProductList/index.tsx
@@ -0,0 +1,14 @@
+import PreviewCard from '@components/PreviewCard';
+import PreviewCardProps from '@type/PreviewCard';
+
+const ProductList = ({ products }: { products: PreviewCardProps[] }) => {
+  return (
+    <div className="grid grid-cols-3 place-items-center gap-x-[8px] gap-y-[24px]">
+      {products.map((product) => (
+        <PreviewCard key={product.id} {...product} type="product" />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
