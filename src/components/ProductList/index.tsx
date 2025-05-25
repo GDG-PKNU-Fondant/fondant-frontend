e3cf9e43f740d24476c4a1ad8b6543d83d69c65d
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-05-26 04:45:59 +0900

    style: ProductList 가로 간격 수정
    - ref: #66

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -3,7 +3,7 @@ import PreviewCardProps from '@type/PreviewCard';
 
 const ProductList = ({ products }: { products: PreviewCardProps[] }) => {
   return (
-    <div className="grid grid-cols-3 place-items-center gap-x-[8px] gap-y-[24px]">
+    <div className="grid grid-cols-3 place-items-center gap-x-[12px] gap-y-[24px]">
       {products.map((product) => (
         <PreviewCard key={product.id} {...product} type="product" />
       ))}
