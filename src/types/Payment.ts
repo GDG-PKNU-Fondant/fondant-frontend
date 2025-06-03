f81498081ebdc29ce1a348fb885eb6b25105fa7a
seun1077 <kseun1118@naver.com>
2025-06-04 02:23:53 +0900

    feat: 결제 타입 정의
    - ref: #72

---
+++ src/types/Payment.ts
@@ -0,0 +1,23 @@
+export type PayMethod =
+  | 'CARD'
+  | 'VIRTUAL_ACCOUNT'
+  | 'TRANSFER'
+  | 'MOBILE'
+  | 'GIFT_CERTIFICATE'
+  | 'EASY_PAY'
+  | 'PAYPAL'
+  | 'ALIPAY'
+  | 'CONVENIENCE_STORE';
+
+export interface OrderProduct {
+  id: number;
+  productName: string;
+  price: number;
+}
+
+export interface RequestPaymentParams {
+  method: string;
+  otherMethod: string;
+  products: OrderProduct[];
+  deliveryCost?: number;
+}
